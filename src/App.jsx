1484b6f2347862017aefa962e749136177c358ad
Ronak khandelwal <136229747+ronakxkhandelwal@users.noreply.github.com>
2024-01-12 11:05:23 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ronak Khandelwal",
+  title: "Web Developer & Content Creator",
+  email: "ronakkhandelwal13@gmail.com",
+  gitHub: "ronakxkhandelwal",
+  instagram: "ronakx77",
+  linkedIn: "ronakkhandelwal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ronak12",
+  youTube: "codewithronak",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/three.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Added by Ronak";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Calculator",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Made by Ronak Khandelwal",
+    url: "https://youtu.be/FCNg8KyMmGI?si=Nx80XaJ0-60yhHM1",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development Tools Made by Ronak Khandelwal",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Made by Ronak Khandelwal",
+    url: "https://youtu.be/FCNg8KyMmGI?si=Nx80XaJ0-60yhHM1",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume Made by Ronak Khandelwal",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Resume made by me ",
+    url: "https://youtu.be/FCNg8KyMmGI?si=Nx80XaJ0-60yhHM1",
   },
   {
     title: "GitHub Codespaces and github.dev",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Made by Ronak Khandelwal",
+    url: "https://youtu.be/FCNg8KyMmGI?si=Nx80XaJ0-60yhHM1",
   },
 ];
 
